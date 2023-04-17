f0a2406c417b532ccedf2f69e1226133b79fb2e1
Konstantin Khoroshilov <manly41142009@yandex.ru>
2023-04-17 16:58:52 +0300

    feat: add solution for task dream-team

--- src/test.js
+++ src/test.js
@@ -1,14 +1,30 @@
-console.log(countCats([
-  [0, 1, '^^'],
-  [0, '^^', 2],
-  ['^^', '^^', 2]
-]));
+// console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']), 'ADMM');
+// console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]), 'LOO');
+console.log(createDreamTeam([
+  ['David Abram'],
+  ['Robin Attfield'],
+  'Thomas Berry',
+  ['Paul R.Ehrlich'],
+  'donna Haraway',
+  ' BrIaN_gOodWiN  ',
+  {
+    0: 'Serenella Iovino'
+  },
+  'Erazim Kohak',
+  '  val_plumwood',
+]), 'BDETV');
+function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    const result = [];
+    members.forEach(member => {
+      if (typeof member === 'string') {
+        const trimmedMember = member.replace(/\s/gi, '');
+        result.push(trimmedMember[0].toUpperCase());
+      }
+    });
 
- 
-function countCats(matrix) {
-  return matrix.reduce((sum, arr) => {
-    return sum += arr.reduce((s, item) => {
-      return item === '^^' ? s + 1 : s;
-    }, 0);
-  }, 0);
+    return result.sort().join('');
+  } else {
+    return false;
+  }
 }
