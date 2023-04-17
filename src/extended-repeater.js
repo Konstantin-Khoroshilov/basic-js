a9b89efbf3b49b8769f918f677817baec5e65d19
Konstantin Khoroshilov <manly41142009@yandex.ru>
2023-04-17 22:43:15 +0300

    feat: add solution for task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const string = str + '';
+  const separator = options.separator != undefined ? options.separator : '+';
+  const addition = options.addition === undefined ? '' : options.addition === null ? 'null' : options.addition + '';
+  const additionSeparator = options.additionSeparator != undefined ? options.additionSeparator : '|';
+  const repeatTimes = options.repeatTimes != undefined ? options.repeatTimes : 1;
+  const additionRepeatTimes = options.additionRepeatTimes != undefined ? options.additionRepeatTimes : 1;
+  const getFullString = (string, separator, repeatTimes, addition = '') => {
+    let result = '';
+    for (let i = 1; i <= repeatTimes; i++) {
+      if (i === repeatTimes) {
+        result += string + addition;
+      } else {
+        result += string + addition + separator;
+      }
+    }
+    return result;
+  }
+  return getFullString(string, separator, repeatTimes, getFullString(addition, additionSeparator, additionRepeatTimes));
 }
 
 module.exports = {
--- src/test.js
+++ src/test.js
@@ -1,24 +1,37 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Calculate turns number and time (in seconds) required
- * to solve puzzle
+ * Create a repeating string based on the given parameters
+ *  
+ * @param {String} str string to repeat
+ * @param {Object} options options object 
+ * @return {String} repeating string
  * 
- * @param {Number} disks number of disks
- * @param {Number} turnsSpeed speed (in turns/hour)
- * @return {Object} object with props turns (number of turns)
- * and seconds (time in seconds)
  *
  * @example
  * 
- * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(disksNumber, turnsSpeed) {
-  return {
-    turns: Math.pow(2, disksNumber) - 1,
-    seconds: Math.floor((Math.pow(2, disksNumber) - 1)/turnsSpeed*3600),
+function repeater(str, options) {
+  const string = str + '';
+  const separator = options.separator != undefined  ? options.separator + '' : '+';
+  const addition = options.addition === undefined ? '' : options.addition === null ? 'null' : options.addition + '';
+  console.log(addition)
+  const additionSeparator = options.additionSeparator != undefined  ? options.additionSeparator + '' : '|';
+  const repeatTimes = options.repeatTimes != undefined  ? options.repeatTimes : 1;
+  const additionRepeatTimes = options.additionRepeatTimes != undefined  ? options.additionRepeatTimes : 1;
+  const getFullString = (string, separator, repeatTimes, addition = '') => {
+    let result = '';
+    for (let i = 1; i <= repeatTimes; i++) {
+      if (i === repeatTimes) {
+        result += string + addition;
+      } else {
+        result += string + addition + separator;
+      }
+    }
+    return result;
   }
+  return getFullString(string, separator, repeatTimes, getFullString(addition, additionSeparator, additionRepeatTimes));
 }
 
-console.log(calculateHanoi(9, 4308), { turns: 511, seconds: 427 })
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }) === 'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null')
