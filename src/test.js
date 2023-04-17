cdeafa77db906c44544e11708477dfae420664f7
Konstantin Khoroshilov <manly41142009@yandex.ru>
2023-04-17 21:33:35 +0300

    feat: add solution for task hanoi-tower

--- src/test.js
+++ src/test.js
@@ -1,28 +1,24 @@
-// // console.log(createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']), 'ADMM');
-// // console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]), 'LOO');
-// console.log(createDreamTeam(new Date(2020, 02, 31)), 'spring');
-// console.log(createDreamTeam(), 'spring');
-// function createDreamTeam(date) {
-//   const currentDate = new Date();
-//   console.log(date.getMonth.toString === currentDate.getMonth.toString)
-//   if (date.getMonth.toString() === currentDate.getMonth.toString() && Date.parse(date)) {
-//     const month = date.getMonth();
-//     let season = 'trololo';
-//     if (month >= 2 && month <= 4) {
-//       season = 'spring';
-//     } else if (month >= 5 && month <= 7) {
-//       season = 'summer';
-//     } else if (month >= 8 && month <= 10) {
-//       season = 'autumn (fall)';
-//     } else if ((month >= 0 && month <= 1) || month === 11) {
-//       season = 'winter';
-//     }
-//     return season;
-//   } else if (date === undefined) {
-//     return 'Unable to determine the time of year!';
-//   } else {
-//     throw new Error('Invalid date!');
-//   }
-// }
-const date = new Date();
-console.log(Object.getOwnPropertyNames(date))
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Calculate turns number and time (in seconds) required
+ * to solve puzzle
+ * 
+ * @param {Number} disks number of disks
+ * @param {Number} turnsSpeed speed (in turns/hour)
+ * @return {Object} object with props turns (number of turns)
+ * and seconds (time in seconds)
+ *
+ * @example
+ * 
+ * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
+ *
+ */
+function calculateHanoi(disksNumber, turnsSpeed) {
+  return {
+    turns: Math.pow(2, disksNumber) - 1,
+    seconds: Math.floor((Math.pow(2, disksNumber) - 1)/turnsSpeed*3600),
+  }
+}
+
+console.log(calculateHanoi(9, 4308), { turns: 511, seconds: 427 })
